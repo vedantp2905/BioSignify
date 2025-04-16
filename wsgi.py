7c23d21fd59c5ac60663f342c8232113532fc910
Vedant Pungliya <vedant29@iastate.edu>
2025-04-15 23:45:52 -0500

    Render

--- wsgi.py
+++ wsgi.py
@@ -8,5 +8,4 @@ sys.path.append(project_root)
 from src.web_interface import app
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port) 
+    app.run() 
